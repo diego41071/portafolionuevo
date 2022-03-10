8ebd9f174fcb063071fc8b70470c5128b1d8f9bd
diego4107112345 <diego@bellpi.com>
2022-03-10 12:20:20 -0500

    cambios en despliegue

--- src/components/HeaderComponent/Header.js
+++ src/components/HeaderComponent/Header.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { HeaderContainer } from "./styles";
 
-export const HeaderComponent = () => {
+export const HeaderComponent = (props) => {
   const [offset, setOffset] = useState(0);
-  const [lang, setLang] = useState("en");
 
   useEffect(() => {
     window.onscroll = () => {
@@ -17,21 +16,23 @@ export const HeaderComponent = () => {
         <div className={`navbar ${offset > 220 && "delay"}`}>
           <img src="https://angry-mcnulty-639e2f.netlify.app/img/da..png" />
           <nav>
-            <a href="">{lang === "es" ? "Inicio" : "Start"}</a>
-            <a href="">{lang === "es" ? "Servicios" : "Services"}</a>
-            <a href="">{lang === "es" ? "Proyectos" : "Projects"} </a>
-            <a href="#index">{lang === "es" ? "Quién Soy" : "Who I am"}</a>
-            <a href="">{lang === "es" ? "Contacto" : "Contact"} </a>
+            <a href="">{props.lang === "es" ? "Inicio" : "Start"}</a>
+            <a href="">{props.lang === "es" ? "Servicios" : "Services"}</a>
+            <a href="">{props.lang === "es" ? "Proyectos" : "Projects"} </a>
+            <a href="#index">
+              {props.lang === "es" ? "Quién Soy" : "Who I am"}
+            </a>
+            <a href="">{props.lang === "es" ? "Contacto" : "Contact"} </a>
           </nav>
           <div className="contentlang">
             <img
               src="https://s1.significados.com/foto/american-flag-1311743-180_sm.png"
-              onClick={() => setLang("en")}
+              onClick={() => props.setLang("en")}
               title="English"
             ></img>
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.svg.png"
-              onClick={() => setLang("es")}
+              onClick={() => props.setLang("es")}
               title="Español"
             ></img>
           </div>
@@ -39,7 +40,7 @@ export const HeaderComponent = () => {
         <div className="title">
           <h1>Diego Ardila</h1>
           <h2 id="index">
-            {lang === "es"
+            {props.lang === "es"
               ? "Diseñador gráfico, UI/UX y desarrollador de software"
               : "Graphic designer, UI/UX and software developer"}
           </h2>
--- src/pages/IndexComponent/Index.js
+++ src/pages/IndexComponent/Index.js
@@ -7,10 +7,11 @@ import { ModalHome } from "../../components/modals/modalHome";
 
 const App = () => {
   const [modal, setModal] = useState(false);
+  const [lang, setLang] = useState("en");
 
   return (
     <StyleIndex className="App">
-      <HeaderComponent />
+      <HeaderComponent lang={lang} setLang={setLang} />
       <div className="flexcolumns">
         <div className="column1">
           <img
@@ -18,55 +19,36 @@ const App = () => {
               "https://disenowebakus.net/imagenes/servicios-web/diseno-de-paginas-web/diseno-web-para-empresas-mexico.jpg"
             }
           ></img>
-          <h1>Proyectos</h1> <h3>Water Pacific</h3>
+          <h1> {lang === "es" ? "Proyectos" : "Proyects"}</h1>
+          <h3>Water Pacific</h3>
           <p>
-            Abel Ángel Quiroz ha inventado un dispositivo llamado Water Pacific,
-            que recoge la humedad del aire y la transforma en agua apta para el
-            consumo humano. El invento ha sido probado en el departamento
-            colombiano de Antioquia, más concretamente en el Valle de Aburrá y,
-            produce 35 litros de agua al día. Water Pacific Se trata de un
-            ingenioso invento que puede suponer una buena solución para el
-            desabastecimiento de agua en algunas zonas, ya que, la humedad
-            relativa en el aire existe hasta en los lugares más áridos del
-            planeta. Además este inventor también ha diseñado el Proyecto
-            Hydroball, capacitado para producir un litro de agua extraída de la
-            humedad del aire, con posibilidad de suministro de al menos diez
-            plantas dentro de un cultivo. Tú también tienes una mente creativa.
-            Desarrolla todo tu potencial inscribiéndote en nuestro Máster en
-            Construcción y Explotación de Obras Hidráulicas.
+            {lang === "es"
+              ? "Abel Ángel Quiroz ha inventado un dispositivo llamado Water Pacific, que recoge la humedad del aire y la transforma en agua apta para el consumo humano. El invento ha sido probado en el departamento colombiano de Antioquia, más concretamente en el Valle de Aburrá y, produce 35 litros de agua al día. Water Pacific Se trata de un ingenioso invento que puede suponer una buena solución para el desabastecimiento de agua en algunas zonas, ya que, la humedad relativa en el aire existe hasta en los lugares más áridos del planeta. Además este inventor también ha diseñado el Proyecto Hydroball, capacitado para producir un litro de agua extraída de la humedad del aire, con posibilidad de suministro de al menos diez plantas dentro de un cultivo. Tú también tienes una mente creativa. Desarrolla todo tu potencial inscribiéndote en nuestro Máster en Construcción y Explotación de Obras Hidráulicas."
+              : "Abel Ángel Quiroz has invented a device called Water Pacific, which collects humidity from the air and transforms it into water suitable for human consumption. The invention has been tested in the Colombian department of Antioquia, more specifically in the Aburrá Valley, and produces 35 liters of water per day. Water Pacific This is an ingenious invention that can be a good solution for the shortage of water in some areas, since the relative humidity in the air exists even in the most arid places on the planet. In addition, this inventor has also designed the Hydroball Project, capable of producing a liter of water extracted from the humidity of the air, with the possibility of supplying at least ten plants within a crop. You also have a creative mind. Develop your full potential by enrolling in our Master's Degree in Construction and Exploitation of Hydraulic Works."}
           </p>
+
           <Button
             onClick={() => {
               setModal(true);
             }}
             secondary
           >
-            Ver todos los proyectos
+            {lang === "es" ? "Ver todos los proyectos" : "See all projects"}
           </Button>
           {modal && <ModalHome setModal={setModal} modal={modal} />}
         </div>
         <div className="column2">
-          <h1>Quién soy </h1>
+          <h1> {lang === "es" ? "Quién soy" : "Who I am"} </h1>
           <p>
-            Soy un desarrollador de software con enfoque en servicios y
-            soluciones, con experiencia en el ámbito de las tecnologías de la
-            información y las comunicaciones. Mi principal compromiso con los
-            clientes es conseguir que las soluciones que ofrezco tengan un
-            impacto positivo en su organización. Ofrezco servicios a la carta
-            atendiendo a las necesidades de cada cliente y abarcando desde los
-            servicios tradicionales hasta las soluciones más innovadoras. Como
-            desarrollador informático, la mejor manera de satisfacer las
-            necesidades del cliente es conocer sus peculiaridades y requisitos y
-            así diseñar la mejor solución posible para cada caso en particular.
-            Ofrecer servicios informáticos de calidad es nuestra meta, dispongo
-            de los productos líderes de cada segmento, así como los fabricantes
-            y proveedores de referencia. Porque nuestro éxito se basa en la
-            confianza que brindan los clientes.
+            {lang === "es"
+              ? "Soy un desarrollador de software con enfoque en servicios y soluciones, con experiencia en el ámbito de las tecnologías de la información y las comunicaciones. Mi principal compromiso con los clientes es conseguir que las soluciones que ofrezco tengan un impacto positivo en su organización. Ofrezco servicios a la carta atendiendo a las necesidades de cada cliente y abarcando desde los servicios tradicionales hasta las soluciones más innovadoras. Como desarrollador informático, la mejor manera de satisfacer las necesidades del cliente es conocer sus peculiaridades y requisitos y así diseñar la mejor solución posible para cada caso en particular. Ofrecer servicios informáticos de calidad es nuestra meta, dispongo de los productos líderes de cada segmento, así como los fabricantes y proveedores de referencia. Porque nuestro éxito se basa en la confianza que brindan los clientes."
+              : "I am a software developer with a focus on services and solutions, with experience in the field of information and communication technologies. My main commitment to clients is to ensure that the solutions I offer have a positive impact on their organization. I offer à la carte services meeting the needs of each client and ranging from traditional services to the most innovative solutions. As a computer developer, the best way to satisfy the client's needs is to know their peculiarities and requirements and thus design the best possible solution for each particular case. Offering quality computer services is our goal, I have the leading products in each segment, as well as the reference manufacturers and suppliers. Because our success is based on the trust that our customers provide."}
           </p>
-          <h1>Servicios</h1>
+          <h1>{lang === "es" ? "Servicios" : "Services"}</h1>
           <p>
-            Creación de páginas web, desarrollo de software de todo tipo,
-            desarrollo móvil, creación de diseños publicitarios.
+            {lang === "es"
+              ? "Creación de páginas web, desarrollo de software de todo tipo, desarrollo móvil, creación de diseños publicitarios."
+              : "Creation of web pages, software development of all kinds, mobile development, creation of advertising designs."}
           </p>
         </div>
       </div>
