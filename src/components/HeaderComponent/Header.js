97311b9d750ac07c354b8533699999f1e5209e10
Diego Ardila <diego@bellpi.com>
2023-02-02 16:25:45 -0500

    correccion menu movil

--- src/components/HeaderComponent/Header.js
+++ src/components/HeaderComponent/Header.js
@@ -49,13 +49,17 @@ export const HeaderComponent = (props) => {
         </div>
         {menu && (
           <div className="menu-mobile">
-            <a href="">{props.lang === "es" ? "Inicio" : "Start"}</a>
-            <a href="">{props.lang === "es" ? "Servicios" : "Services"}</a>
-            <a href="">{props.lang === "es" ? "Proyectos" : "Projects"} </a>
-            <a href="#index">
-              {props.lang === "es" ? "Quién Soy" : "Who I am"}
+            <a href="#index">{props.lang === "es" ? "Inicio" : "Start"}</a>
+            <a href="#services">
+              {props.lang === "es" ? "Servicios" : "Services"}
             </a>
-            <a href="">{props.lang === "es" ? "Contacto" : "Contact"} </a>
+            <a href="#proyects">
+              {props.lang === "es" ? "Proyectos" : "Projects"}{" "}
+            </a>
+            <a href="#who">{props.lang === "es" ? "Quién Soy" : "Who I am"}</a>
+            <a href="#footer">
+              {props.lang === "es" ? "Contacto" : "Contact"}{" "}
+            </a>
           </div>
         )}
         <div className="title">
