68ec771214dad0280b0cd4a3b047768a82eacf58
diego41071 <alexander.diego.2007@hotmail.com>
2023-10-30 22:12:11 -0500

    despliegue

--- src/components/FooterComponent/styles.js
+++ src/components/FooterComponent/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const FooterContainer = styled.div`
   footer {
     background: #f7f7f7;
-    padding: 92px;
+    padding: 73px;
     & .flexicons {
       display: flex;
       align-items: center;
